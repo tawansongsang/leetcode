13676440a7c854f36dcdb7edcfd08cb32904aedf
tawansongsang <tawansongsang.k@gmail.com>
2024-03-22 22:12:53 +0700

    finish 83_remove_duplicate

--- crates/easy/83_remove_duplicates_from_sorted_list/src/lib.rs
+++ crates/easy/83_remove_duplicates_from_sorted_list/src/lib.rs
@@ -12,44 +12,84 @@ impl ListNode {
     }
 }
 
-pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     if head == None {
         return None;
     }
-    let node_head = head.as_ref().unwrap();
-    let mut node_current = node_head.clone();
-    let mut node_next = node_head.next.as_ref();
-    while !node_next.is_none() {
-        if node_current.val == node_next.unwrap().val {
-            node_next = node_next.unwrap().next.as_ref();
-            continue;
+    let mut current_opt = head.as_mut();
+    while let Some(current) = current_opt {
+        let mut next_opt = current.next.take();
+
+        while let Some(next) = next_opt.as_mut() {
+            if current.val == next.val {
+                next_opt = next.next.take();
+            } else {
+                current.next = next_opt;
+                break;
+            }
         }
 
-        node_current.next = node_next.cloned();
-        node_current = node_next.unwrap().clone();
-        node_next = node_next.unwrap().next.as_ref();
+        current_opt = current.next.as_mut();
     }
 
     head
 }
 
+pub fn delete_duplicates_best_time(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut cur = &mut head;
+    while let Some(node) = cur {
+        while let Some(next) = &mut node.next {
+            if node.val == next.val {
+                node.next = next.next.take();
+            } else {
+                break;
+            }
+        }
+        cur = &mut node.next;
+    }
+    head
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
     fn test_1() {
         let mut in_node_1 = Box::new(ListNode::new(1));
         let mut in_node_2 = Box::new(ListNode::new(1));
-        in_node_1.next = Some(in_node_2.clone());
         let in_node_3 = Box::new(ListNode::new(2));
-        in_node_2.next = Some(in_node_3.clone());
-        let input = in_node_1.clone();
-        let result = delete_duplicates(Some(input));
+        in_node_2.next = Some(in_node_3);
+        in_node_1.next = Some(in_node_2);
+        println!("{:?}", in_node_1);
+        let result = delete_duplicates(Some(in_node_1));
         let mut o_node_1 = Box::new(ListNode::new(1));
         let o_node_2 = Box::new(ListNode::new(2));
-        o_node_1.next = Some(o_node_2.clone());
-        let output = Some(o_node_1.clone());
-        assert_eq!(result, output);
+        o_node_1.next = Some(o_node_2);
+        assert_eq!(result, Some(o_node_1));
+        // assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_2() {
+        let mut in_node_1 = Box::new(ListNode::new(1));
+        let mut in_node_2 = Box::new(ListNode::new(1));
+        let mut in_node_3 = Box::new(ListNode::new(2));
+        let mut in_node_4 = Box::new(ListNode::new(3));
+        let in_node_5 = Box::new(ListNode::new(3));
+        in_node_4.next = Some(in_node_5);
+        in_node_3.next = Some(in_node_4);
+        in_node_2.next = Some(in_node_3);
+        in_node_1.next = Some(in_node_2);
+        println!("{:?}", in_node_1);
+        let result = delete_duplicates(Some(in_node_1));
+        let mut o_node_1 = Box::new(ListNode::new(1));
+        let mut o_node_2 = Box::new(ListNode::new(2));
+        let o_node_3 = Box::new(ListNode::new(3));
+        o_node_2.next = Some(o_node_3);
+        o_node_1.next = Some(o_node_2);
+        assert_eq!(result, Some(o_node_1));
+        // assert_eq!(result, None);
     }
 }
