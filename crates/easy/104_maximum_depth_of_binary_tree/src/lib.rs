1e0255602ecfdbc9aed4cd8939f7b2f61e12a9f5
tawansongsang <tawansongsang.k@gmail.com>
2024-09-21 14:01:35 +0700

    finish easy 104 maximum depth of binary tree

--- crates/easy/104_maximum_depth_of_binary_tree/src/lib.rs
+++ crates/easy/104_maximum_depth_of_binary_tree/src/lib.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::cmp::max;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -19,8 +20,20 @@ impl TreeNode {
     }
 }
 
+pub fn depth_first_search(node: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match node {
+        Some(inner) => {
+            max(
+                depth_first_search(&inner.borrow().left),
+                depth_first_search(&inner.borrow().right),
+            ) + 1
+        }
+        None => 0,
+    }
+}
+
 pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    0
+    depth_first_search(&root)
 }
 
 #[cfg(test)]
@@ -30,15 +43,31 @@ mod tests {
     #[test]
     fn test_1() {
         let root = Some(Rc::new(RefCell::new(TreeNode::new(3))));
-        let layer_one_left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        let layer_one_l = Some(Rc::new(RefCell::new(TreeNode::new(9))));
+        let layer_one_r = Some(Rc::new(RefCell::new(TreeNode::new(20))));
+        let layer_two_l = Some(Rc::new(RefCell::new(TreeNode::new(15))));
+        let layer_two_r = Some(Rc::new(RefCell::new(TreeNode::new(7))));
+
+        if let Some(inner) = &layer_one_r {
+            inner.borrow_mut().left = layer_two_l;
+            inner.borrow_mut().right = layer_two_r;
+        }
+        if let Some(inner) = &root {
+            inner.borrow_mut().left = layer_one_l;
+            inner.borrow_mut().right = layer_one_r;
+        }
         let result = max_depth(root);
-        assert_eq!(result, 0);
+        assert_eq!(result, 3);
     }
 
     #[test]
     fn test_2() {
         let root = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        let layer_one_r = Some(Rc::new(RefCell::new(TreeNode::new(2))));
+        if let Some(inner) = &root {
+            inner.borrow_mut().right = layer_one_r;
+        }
         let result = max_depth(root);
-        assert_eq!(result, 0);
+        assert_eq!(result, 2);
     }
 }
